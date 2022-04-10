678ffb555a2bc6086ec11aed826007e41c631e2f
草鞋没号 <308487730@qq.com>
2022-04-10 09:09:42 +0800

    update config

--- vite-electron.config.ts
+++ vite-electron.config.ts
@@ -1,11 +1,12 @@
+import path from 'path'
 import { defineConfig } from 'vite-plugin-electron'
 
 export default defineConfig({
   main: {
-    entry: 'electron-main.ts',
-    nodeIntegration: true,
+    entry: 'electron-main/index.ts',
   },
   preload: {
-    entry: 'electron-preload.ts',
+    // Must be use absolute path, this is the limit of rollup
+    input: path.join(__dirname, 'electron-preload/index.ts'),
   },
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import electron from 'vite-plugin-electron'
+import electronRenderer from 'vite-plugin-electron/renderer'
 import electronConfig from './vite-electron.config'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     electron(electronConfig),
+    electronRenderer(),
   ],
 })
